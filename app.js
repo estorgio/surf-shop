7393f7ffd6d97d2e9b9063d8d68214b5ef7e0776
Fortunato Estorgio <contact@estorgio.com>
2019-06-26 02:59:45 +0800

    Post resource

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const passport = require('passport');
 const User = require('./models/user');
 const session = require('express-session');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 
 // Require routes
 const indexRouter = require('./routes/index');
@@ -29,9 +30,10 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'));
 
 // Configure passport and sessions
 app.use(session({
